15ffb5a40439382cf04e8468a568b4534de0a835
Vijayalayan Rajasekaran <vijayalayan.rajasekaran@outlook.com>
2025-05-11 15:53:16 +0200

    Removed unncessary assemblies for the project

--- CleanArchitectureBlazor/Core/Domain/Entities/ToDoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core.Domain.Entities
-{
-    public class ToDoItem
-    {
-        public Guid Id { get; set; }  = Guid.NewGuid();
-        public  string Title { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
